79af1aa07331eda41ddeeee7f0e2e1214da07a54
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-29 00:16:46 +0200

    please work

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
 # Install Flask from pip3
-package { 'flask==2.1.0':
-  ensure   => install,
+package { 'Python3':
+  ensure   => '3.8.10',
   provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
