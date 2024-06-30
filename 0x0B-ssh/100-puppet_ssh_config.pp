fecad8e500e1d5427debaf38767634f5d0588162
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-01 01:39:00 +0200

    fine

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
 exec { 'configure_ssh_client':
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config,
-   echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  path    => '/usr/bin',
 }
