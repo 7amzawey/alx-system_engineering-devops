2a0754388f20dc4fb49a9204b4c8ca8d2785d7b7
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-01 01:44:23 +0200

    nice

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+# this one is gonna use puppet
 exec { 'configure_ssh_client':
   command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config; echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => '/usr/bin',
