85da67539bd9d5714d80193f4bec17c7adf388c1
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-01 01:34:49 +0200

    let's see

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-# Configures SSH client to use the private key ~/.ssh/school and refuse password authentication
-exec {'ssh_command':
-  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config;
-  echo "PasswordAuthentication no" >> /etc/ssh/ssh_config'}
+exec { 'configure_ssh_client':
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config,
+   echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+}
