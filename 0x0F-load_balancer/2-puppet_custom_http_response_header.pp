b61b4f3c5ced27ea827606dae09959111c420904
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-20 11:45:52 +0200

    changes

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,27 @@
-# this file is to make sure that this line is added to the config file
+# Install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
-service { 'nignx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+# Ensure the Nginx service is running and enabled
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
 }
 
+# Ensure the custom header line is present in the Nginx configuration
 file_line { 'add_header':
-  path => '/etc/nginx/sites-available/default',
-  line => 'add_header X-Served-By $hostname;',
+  path  => '/etc/nginx/sites-available/default',
+  line  => 'add_header X-Served-By $hostname;',
+  match => 'add_header X-Served-By',
+  notify => Service['nginx'],
+}
+
+# Reload Nginx to apply changes
+exec { 'reload_nginx':
+  command     => '/usr/sbin/nginx -s reload',
+  refreshonly => true,
+  subscribe   => File_line['add_header'],
 }
 
