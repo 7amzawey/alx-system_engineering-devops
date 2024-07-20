223a858ce3e17b91ba4ff0725454096ac463940c
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-20 11:27:31 +0200

    2-puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,14 @@
 # this file is to make sure that this line is added to the config file
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nignx':
+  ensure  => running,
+  enable  => true,
+  require => package['nginx'],
+}
+
 file_line { 'add_header':
   path => '/etc/nginx/sites-available/default',
   line => 'add_header X-Served-By $hostname;',
