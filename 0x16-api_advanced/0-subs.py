cef182a8ed86cb3f515fb3301b8617cfc5d9fd36
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-15 10:49:31 +0200

    update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """quering the number of subscribers in a specific subreddit"""
-    url = f"https://reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return response.json().get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+    """number_of_subscribers
+    queries the Reddit API and returns the number of subscribers
+    If not a valid subreddit, return 0."""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, allow_redirects=False)
+    if response.ok:
+        return response.json().get('data').get('subscribers')
+    else:
         return 0
