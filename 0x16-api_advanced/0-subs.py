22587751f540278437d0ed83febd58ba26d2b89e
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-15 10:45:36 +0200

    some updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,5 +9,11 @@ def number_of_subscribers(subreddit):
     """quering the number of subscribers in a specific subreddit"""
     url = f"https://reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers)
-    return response.json().get('data', {}).get('subscribers', 0)
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json().get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
