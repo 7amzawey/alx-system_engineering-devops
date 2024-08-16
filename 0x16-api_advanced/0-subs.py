b2713ac8dfaa2e4dca4b19e556e3538e64c81583
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-17 01:59:50 +0200

    count it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,8 @@
 def number_of_subscribers(subreddit):
     """ Returns subscriber count of subreddit or 0 """
     from requests import get
-
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
     headers = {'user-agent': 'my-app/0.0.1'}
-
     r = get(url, headers=headers, allow_redirects=False)
 
     if r.status_code != 200:
