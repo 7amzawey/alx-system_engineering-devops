1f0dc4f645db22a5689f8547ade3d9c82aee11fe
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-17 01:20:01 +0200

    fine

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-This is a module for quering a string
+This is a module for querying a string
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """number_of_subscribers
-    queries the Reddit API and returns the number of subscribers
+    Queries the Reddit API and returns the number of subscribers
     If not a valid subreddit, return 0."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers,  allow_redirects=False)
-    if response.ok:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         return response.json().get('data', {}).get('subscribers', 0)
     else:
         return 0
