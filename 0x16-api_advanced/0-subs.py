20d28b3ec209969a574bc0dc23e681f6bfb4e34a
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-17 01:18:23 +0200

    plz work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,9 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     If not a valid subreddit, return 0."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers,  allow_redirects=False)
     if response.ok:
-        return response.json().get('data').get('subscribers')
+        return response.json().get('data', {}).get('subscribers', 0)
     else:
         return 0
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A recursive function that queries the Reddit API and returns
+a list containing titles of all hot posts from a subreddit."""
+import requests
+
+
+def recurse(subreddit, hot_list=None):
+    """Recursively fetches all hot post titles from a subreddit."""
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    data = response.json().get('data', {})
+    posts = data.get('children', [])
+    titles = [post.get('data', {}).get('title', '') for post in posts]
+    if not posts:
+        return hot_list
+    hot_list.extend(titles)
+    after = data.get('after', None)
+    if after:
+        next_url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+        return recurse(subreddit, hot_list)
+    return hot_list
