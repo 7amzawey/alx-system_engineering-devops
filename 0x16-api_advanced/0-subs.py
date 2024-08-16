17392b300da9a623101b8a0e96b76c4920da6fef
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-17 01:24:27 +0200

    let's get this don

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-This is a module for querying a string
+Return the total number of subscribers.
 """
+
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """number_of_subscribers
-    Queries the Reddit API and returns the number of subscribers
-    If not a valid subreddit, return 0."""
+    """Return the total number of subscirbers."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
