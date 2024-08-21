dcc3b74ae280cc1497d5373f422bb04326a87d75
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-21 18:50:00 +0200

    fix error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-#fix server internal error
+# Fixes the internel error.
 
-exec { 'fix_error' :
+exec { 'fix-error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
